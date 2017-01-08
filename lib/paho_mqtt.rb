05af23f0a6dd142539012097065c5cc84748c984
Pierre Goudet <p-goudet@ruby-dev.jp>
2017-01-08 09:28:36 +0900

    Correcting missing files

--- lib/paho_mqtt.rb
+++ lib/paho_mqtt.rb
@@ -1,6 +1,6 @@
-require "paho.mqtt/version"
-require "paho.mqtt/client"
-require "paho.mqtt/packet"
+require "paho_mqtt/version"
+require "paho_mqtt/client"
+require "paho_mqtt/packet"
 
 module PahoMqtt
 
