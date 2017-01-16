13d76b778195d23035fb5af87f2dd3b1673efb3d
Pierre Goudet <p-goudet@ruby-dev.jp>
2017-01-16 09:29:35 +0900

    Fix packet module

---
+++ lib/paho_mqtt/packet.rb
@@ -0,0 +1,19 @@
+# encoding: BINARY
+require "packet/base"
+require "packet/connect"
+require "packet/connack"
+require "packet/publish"
+require "packet/puback"
+require "packet/pubrec"
+require "packet/pubrel"
+require "packet/pubcomp"
+require "packet/subscribe"
+require "packet/suback"
+require "packet/unsubscribe"
+require "packet/unsuback"
+require "packet/pingreq"
+require "packet/pingresp"
+require "packet/disconnect"
+
+module PahoMqtt
+end
