69d75dd6b72b7de5755231fa11d3cef373aec5d8
Pierre Goudet <p-goudet@ruby-dev.jp>
2017-04-03 09:49:29 +0900

    Add copyright to Nicholas Humfrey

--- lib/paho_mqtt/packet/connack.rb
+++ lib/paho_mqtt/packet/connack.rb
@@ -1,17 +1,35 @@
 # encoding: BINARY
+### original file from the ruby-mqtt gem
+### located at https://github.com/njh/ruby-mqtt/blob/master/lib/mqtt/packet.rb
+###
+### The MIT License (MIT)
 
+### Copyright (c) 2009-2013 Nicholas J Humfrey
+
+### Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without
+
+### restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+
+### Software is furnished to do so, subject to the following conditions:
+
+### The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software
+###
+### THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+### WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+### OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+### OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 module PahoMqtt
   module Packet
     class Connack < PahoMqtt::Packet::Base
       # Session Present flag
       attr_accessor :session_present
-      
+
       # The return code (defaults to 0 for connection accepted)
       attr_accessor :return_code
 
       # Default attribute values
       ATTR_DEFAULTS = {:return_code => 0x00}
-      
+
       # Create a new Client Connect packet
       def initialize(args={})
         # We must set flags before other attributes
@@ -81,4 +99,3 @@ module PahoMqtt
     end
   end
 end
-
--- lib/paho_mqtt/packet/connect.rb
+++ lib/paho_mqtt/packet/connect.rb
@@ -1,4 +1,23 @@
 # encoding: BINARY
+### original file from the ruby-mqtt gem
+### located at https://github.com/njh/ruby-mqtt/blob/master/lib/mqtt/packet.rb
+###
+### The MIT License (MIT)
+
+### Copyright (c) 2009-2013 Nicholas J Humfrey
+
+### Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without
+
+### restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+
+### Software is furnished to do so, subject to the following conditions:
+
+### The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software
+###
+### THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+### WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+### OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+### OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 module PahoMqtt
   module Packet
@@ -73,7 +92,7 @@ module PahoMqtt
         if @keep_alive < 0
           raise "Invalid keep-alive value: cannot be less than 0"
         end
-        
+
         body += encode_flags(@connect_flags)
         body += encode_short(@keep_alive)
         body += encode_string(@client_id)
