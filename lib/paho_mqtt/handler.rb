37f8e1a2d6c04ec70ac974fe8aa7d67c91e9ff0b
Vishnu <vishnu@yubi.in>
2018-02-21 16:17:04 +0200

    Fixed the typon in code where 'topcis' was written instead of 'topics'. This was causing call to 'unsubscribe' crash

--- lib/paho_mqtt/handler.rb
+++ lib/paho_mqtt/handler.rb
@@ -18,7 +18,7 @@ module PahoMqtt
     attr_reader :registered_callback
     attr_accessor :last_ping_resp
     attr_accessor :clean_session
-    
+
     def initialize
       @registered_callback = []
       @last_ping_resp = -1
@@ -46,7 +46,7 @@ module PahoMqtt
           else
             handle_packet(packet)
             @last_ping_resp = Time.now
-          end          
+          end
         end
       end
     end
@@ -133,7 +133,7 @@ module PahoMqtt
       id = packet.id
       topics = []
       topics = @subscriber.remove_subscription(id, topics)
-      unless topcis.empty?
+      unless topics.empty?
         puts "PAHO TOPICS: #{topics}"
         @on_unsuback.call(topics) unless @on_unsuback.nil?
       end
@@ -157,7 +157,7 @@ module PahoMqtt
 
     def handle_pubrec(packet)
       id = packet.id
-      if @publisher.do_pubrec(id) == MQTT_ERR_SUCCESS      
+      if @publisher.do_pubrec(id) == MQTT_ERR_SUCCESS
         @on_pubrec.call(packet) unless @on_pubrec.nil?
       end
     end
