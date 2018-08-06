c0d822f69309ad5ae208b2c42427b7abb00345f3
oldgreen <oldoldgreen@gmail.com>
2018-08-06 10:22:09 +0200

    Remove useless method argument passing.

--- lib/paho_mqtt/client.rb
+++ lib/paho_mqtt/client.rb
@@ -186,7 +186,7 @@ module PahoMqtt
     end
 
     def loop_misc
-      if @connection_helper.check_keep_alive(@persistent, @handler.last_ping_resp, @keep_alive) == MQTT_CS_DISCONNECT
+      if @connection_helper.check_keep_alive(@persistent, @keep_alive) == MQTT_CS_DISCONNECT
         reconnect if check_persistence
       end
       @publisher.check_waiting_publisher
